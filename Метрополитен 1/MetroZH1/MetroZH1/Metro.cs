69bc9a35d5f1e1c77d32faacdcdf3f12df85d312
WonderWaffle4 <zhmelev.g@gmail.com>
2021-11-08 18:33:14 +0300

    1 часть Задания 1 метрополитена выполнено

---
+++ Метрополитен 1/MetroZH1/MetroZH1/Metro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroZH1
+{
+    public class Metro
+    {
+        private List<Line> lines;
+        private string city;
+
+        public Metro(string city)
+        {
+
+        }
+        public string GetCity()
+        {
+
+        }
+        public void AddLine(string name, string color)
+        {
+
+        }
+        public void RemoveLine(string name)
+        {
+
+        }
+        public List<Station> FindStation(string name)
+        {
+
+        }
+        public Station FindStation(string name, string lineName)
+        {
+
+        }
+        public List<Station> GetStationList(string name)
+        {
+
+        }
+        public void LoadStationsFromFile(string filename)
+        {
+
+        }
+    }
+}
---
+++ Метрополитен 1/MetroZH1/MetroZH1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetroZH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
